c9d9595043782a3b1c43947de7a80f7b3307d3ca
marco.jesus <marco.jesus@smbh-020441.sysmap.com.br>
2017-01-25 19:16:39 -0200

    teste1.2

--- src/main/java/com/javacodegeeks/enterprise/rest/jersey/JerseyFileUpload.java
+++ src/main/java/com/javacodegeeks/enterprise/rest/jersey/JerseyFileUpload.java
@@ -5,13 +5,16 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import com.sun.jersey.core.header.FormDataContentDisposition;
-import com.sun.jersey.multipart.FormDataParam;
+
+import com.sun.jersey.core.header.ContentDisposition;
+import com.sun.jersey.multipart.FormDataBodyPart;
+import com.sun.jersey.multipart.FormDataMultiPart;
 
 @Path("/files")
 public class JerseyFileUpload {
@@ -25,27 +28,31 @@ public class JerseyFileUpload {
 	@POST
 	@Path("/upload")
 	@Consumes(MediaType.MULTIPART_FORM_DATA)
-	public Response uploadFile(
-			@FormDataParam("file") InputStream fileInputStream,
-			@FormDataParam("file") FormDataContentDisposition contentDispositionHeader) {
+	public Response uploadFile(FormDataMultiPart form) {
 
-		String filePath = SERVER_UPLOAD_LOCATION_FOLDER	+ contentDispositionHeader.getFileName();
+		 FormDataBodyPart filePart = form.getField("file");
+
+		 ContentDisposition headerOfFilePart =  filePart.getContentDisposition();
+
+		 InputStream fileInputStream = filePart.getValueAs(InputStream.class);
+
+		 String filePath = SERVER_UPLOAD_LOCATION_FOLDER + headerOfFilePart.getFileName();
 
 		// save the file to the server
 		saveFile(fileInputStream, filePath);
 
-		String output = "File saved to server location : " + filePath;
+		String output = "File saved to server location using FormDataMultiPart : " + filePath;
 
 		return Response.status(200).entity(output).build();
 
 	}
 
 	// save uploaded file to a defined location on the server
-	private void saveFile(InputStream uploadedInputStream,
-			String serverLocation) {
+	private void saveFile(InputStream uploadedInputStream, String serverLocation) {
 
 		try {
-			OutputStream outpuStream = new FileOutputStream(new File(serverLocation));
+			OutputStream outpuStream = new FileOutputStream(new File(
+					serverLocation));
 			int read = 0;
 			byte[] bytes = new byte[1024];
 
@@ -53,8 +60,11 @@ public class JerseyFileUpload {
 			while ((read = uploadedInputStream.read(bytes)) != -1) {
 				outpuStream.write(bytes, 0, read);
 			}
+
 			outpuStream.flush();
 			outpuStream.close();
+
+			uploadedInputStream.close();
 		} catch (IOException e) {
 
 			e.printStackTrace();
